dc9525175e11ddf596bd59760ff42c2b906e53b7
reyzakomalaa <reyza.pratama1001@gmail.com>
2023-02-03 21:50:18 +0700

    change chromedriver using maven repository

--- src/test/java/step/home.java
+++ src/test/java/step/home.java
@@ -1,17 +1,10 @@
 package step;
 
 import config.env;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import object.pageHome;
-import object.pagePIM;
-import object.reuseableObject;
-import org.junit.Assert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.Random;
 
 public class home extends env {
     pageHome elementHome = new pageHome();
--- src/test/java/step/hooks.java
+++ src/test/java/step/hooks.java
@@ -3,14 +3,18 @@ package step;
 import config.env;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class hooks extends env {
     @Before
     public void before() {
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
-        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions opt = new ChromeOptions();
+        opt.setHeadless(false);
+        driver = new ChromeDriver(opt);
         driver.get("http://employee.sdcillsy.my.id/");
         driver.manage().window().maximize();
     }
